0efa3831913f7c9db3ae4b6ab77c3e6cd5b2e4ec
AdrianDeV <adrianmvparedes@gmail.com>
2024-10-04 20:33:25 +0200

    Agregados estilos

---
+++ front/seismic/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Incluir todos los archivos HTML y TS en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
