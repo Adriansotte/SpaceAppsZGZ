e82072a9aa6d2315df1e92bd6c0fc7c98e11e0bb
AdrianDeV <adrianmvparedes@gmail.com>
2024-10-04 20:51:21 +0200

    Ajustado PrimeNG

--- front/seismic/src/app/app.module.ts
+++ front/seismic/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FileUploadModule } from 'primeng/fileupload';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
